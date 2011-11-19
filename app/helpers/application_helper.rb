fc71cbb8089bc27a5b07768c6da825e55af574e9
Saraswati <ohmysaraswati@gmail.com>
2011-11-20 02:17:27 +0400

    Transfer logo's html from layout to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
